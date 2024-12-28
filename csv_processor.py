1bf2826075f536ea2ff5b5cca0c08a924f148fca
Olga Szymczak <140915825+clouddiish@users.noreply.github.com>
2024-12-28 16:47:05 +0100

    write_file()

--- csv_processor.py
+++ csv_processor.py
@@ -7,16 +7,24 @@ def read_file(filename):
             reader = csv.DictReader(f)
             data = [row for row in reader]
             return data
+
     except FileNotFoundError:
         print(f"File {filename} does not exist")
+
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 def is_num(value):
     try:
         float(value)
         return True
+
     except ValueError:
         return False
+
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 def process_data(dict):
@@ -41,14 +49,38 @@ def process_data(dict):
     except TypeError:
         print("Provided data must be a dictionary")
 
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+def write_file(filename, data):
+    try:
+        with open(filename, "w", newline="") as f:
+            headers = list(data[0].keys())
+            writer = csv.DictWriter(f, fieldnames=headers)
+            writer.writerows(data)
+    except Exception as e:
+        print(f"Error: {e}")
+
 
 def main():
-    data = read_file("data.csv")
-    if data:
-        print(process_data(data))
+    input_file = "data.csv"
+    output_file = "output_data.csv"
 
-    else:
+    data = read_file(input_file)
+
+    if not data:
         print("File is empty or its content is not valid csv")
+        return
+
+    processed_data = process_data(data)
+
+    if not processed_data:
+        print("No data to process")
+        return
+
+    write_file(output_file, processed_data)
+    print(f"Processed data saved to {output_file}")
 
 
 if __name__ == "__main__":
